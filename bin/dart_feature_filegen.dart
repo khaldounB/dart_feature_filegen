13b4e7b69f485f067834e75ef44adf4bddcfc81f
Khaldoun Badreah <102937196+khaldounB@users.noreply.github.com>
2024-08-30 13:35:18 +0300

    Update dart_feature_filegen.dart

--- bin/dart_feature_filegen.dart
+++ bin/dart_feature_filegen.dart
@@ -1,12 +1,11 @@
 import 'package:dart_feature_filegen/dart_feature_filegen.dart';
 
 void main(List<String> arguments) async {
-  if (arguments.length < 2) {
-    print('Usage: dart run dart_feature_filegen <app> <feature_name>');
+  if (arguments.length < 1) {
+    print('Usage: dart run dart_feature_filegen <feature_name>');
     return;
   }
 
-  final app = arguments[0];
-  final featureName = arguments[1];
-  await generateFeature(featureName,app);
+  final featureName = arguments[0];
+  await generateFeature(featureName);
 }
